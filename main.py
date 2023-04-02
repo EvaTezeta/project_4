ba5675f217dd285b55edb71ca8888e71e355566e
EvaTezeta <118466414+EvaTezeta@users.noreply.github.com>
2023-04-02 11:11:45 +0000

    Working grid and new random field

--- main.py
+++ main.py
@@ -6,105 +6,87 @@ import random
 #Make class
 class Grid:
 
-    def __init__(self, row, column):
-        self.row = row
-        self.col = column
+  def __init__(self, parent):
+    self.parent = parent
+    self.row = 5
+    self.col = 5
+    self.root = tk.Frame(parent)
+    self.root.pack()
 
-        # Create a 5x5 grid with random numbers
-        self.grid = [[random.randint(0, 9) for j in range(self.col)]
-                     for i in range(self.row)]
+    # Create a 5x5 grid with random numbers
+    self.grid = [[random.randint(0, 9) for j in range(self.col)]
+                 for i in range(self.row)]
 
-    def generate(self):
-        for i in range(5):
-            for j in range(5):
-                button = tk.Button(self.root, text="")
-                button = tk.Button(self.root, text=random.randint(0, 9))
-                button["width"] = 8
-                button["height"] = 3
-                button.grid(row=i, column=j)
+    self.buttons = []
+    for i in range(self.row):
+      row = []
+      for j in range(self.col):
+        button = tk.Button(self.root, text=self.grid[i][j])
+        button["width"] = 8
+        button["height"] = 3
+        button.grid(row=i, column=j)
+        row.append(button)
+      self.buttons.append(row)
 
-    def set_start(self, row, column):
-        button = self.root.grid_slaves(row=row, column=column)[0]
-        button.config(text="S")
+  def set_start(self, row, column):
+    self.buttons[row][column].config(text="S")
 
-    def set_goal(self, row, column):
-        button = self.root.grid_slaves(row=row, column=column)[0]
-        button.config(text="G")
+  def set_goal(self, row, column):
+    self.buttons[row][column].config(text="G")
 
-    def calculate_maximal_score(self):
-        pass
+  def calculate_maximal_score(self):
+    pass
 
-    def choose_the_best_root(self):
-        pass
+  def choose_the_best_root(self):
+    pass
 
-    def __init__(self, parent):
-        self.parent = parent
+  def pathbuttonClick(self, event):
+    pass
 
-        self.container1 = tk.Frame(parent)
-        self.container1.pack()
+  def fieldbuttonClick(self, event):
+    self.grid = [[random.randint(0, 9) for j in range(self.col)]
+                 for i in range(self.row)]
+    for i in range(self.row):
+      for j in range(self.col):
+        self.buttons[i][j].config(text=self.grid[i][j])
 
-        self.scoretext = tk.Label(self.container1)
-        self.scoretext.configure(text="Maximal possible score: ",
-                                 background="white")
-        self.scoretext.pack(side="left")
-
-        self.container2 = tk.Frame(parent)
-        self.container2.pack()
-
-        self.pathbutton = tk.Button(self.container2)
-        self.pathbutton["text"] = "Find best path"
-        self.pathbutton.pack(side="left")
-        self.pathbutton.bind("<Button-1>", self.pathbuttonClick)
-
-        self.fieldbutton = tk.Button(self.container2,
-                                     text="Create new random field")
-        self.fieldbutton.pack(side="left")
-        self.fieldbutton.bind("<Button-2>", self.fieldbuttonClick)
-
-        self.exitbutton = tk.Button(self.container2, text="Exit program")
-        self.exitbutton.pack(side="left")
-        self.exitbutton.bind("<Button-3>", self.exitbuttonClick)
-
-    def generateNewField(self):
-        rows = 10
-        columns = 10
-        field = []
-        for i in range(rows):
-            row = []
-            for j in range(columns):
-                row.append(random.randint(0, 9))
-            field.append(row)
-        return field
-
-    def pathbuttonClick(self, event):
-        self.hwtext.configure()
-        #This button should generate best path
-
-    def fieldbuttonClick(self, event):
-        new_field = self.generateNewField()
-        self.scoretext.configure(
-            text=
-            f"Maximal possible score: {sum([max(row) for row in new_field])}",
-            background="white")
-
-    # This button exits the program (should already work)
-    def exitbuttonClick(self, event):
-        self.parent.destroy()
+  # This button exits the program
+  def exitbuttonClick(self, event):
+    self.parent.destroy()
 
 
-#Root should be at bottom of all code
+# Geometry and title
 root = tk.Tk()
 root.geometry("500x500")
+root.title("Best Path")
 
 # Create a frame to hold the grid
 frame = tk.Frame(root)
-# 80 pixels depends on your systems font size, 80px is a gamble
-frame.grid(row=0, column=0, padx=80, pady=0)
+frame.pack()
 
 # Create the grid within the frame
 Grid = Grid(frame)
-Grid.generate()
 Grid.set_start(4, 0)
 Grid.set_goal(0, 4)
+
+# Create a container for the buttons
+container = tk.Frame(root)
+container.pack()
+
+# Create the Find best path button
+pathbutton = tk.Button(container)
+pathbutton["text"] = "Find best path"
+pathbutton.pack(side="left")
+pathbutton.bind("<Button-1>", Grid.pathbuttonClick)
+
+# Create the Create new random field button
+fieldbutton = tk.Button(container, text="Create new random field")
+fieldbutton.pack(side="left")
+fieldbutton.bind("<Button-1>", Grid.fieldbuttonClick)
+
+# Create the Exit program button
+exitbutton = tk.Button(container, text="Exit program")
+exitbutton.pack(side="left")
+exitbutton.bind("<Button-1>", Grid.exitbuttonClick)
 
 root.mainloop()
