fdda7833d00ece5056ee77f3a004efdba2c96e88
EvaTezeta <118466414+EvaTezeta@users.noreply.github.com>
2023-04-07 10:20:52 +0000

    Best path changes

--- CoTaPP.py
+++ CoTaPP.py
@@ -9,71 +9,68 @@ class Grid:
 
   def __init__(self, parent):
     self.parent = parent
+    self.root = tk.Frame(parent)
     self.row = 5
     self.col = 5
-    self.root = tk.Frame(parent)
     self.root.pack()
-    
-    # Create a frame to hold the grid
-    self.frame = tk.Frame(parent)
-    self.frame.pack()
-    
-    # Create a container for the buttons
-    self.container = tk.Frame(parent)
-    self.container.pack()
-    
-    # Print the maximum score
-    self.score_label = tk.Label(self.container)
-    self.score_label["text"] = self.get_maximum_possible_score()
-    self.score_label.configure(text=self.score_label["text"])
-    self.score_label.pack(side="top")
-    
-    # Create the Find best path button
-    self.pathbutton = tk.Button(self.container)
-    self.pathbutton["text"] = "Find best path"
-    self.pathbutton.pack(side="left")
-    self.pathbutton.bind("<Button-1>", self.pathbuttonClick)
+    self.cell_size = 60
+    self.canvas_width = self.col * self.cell_size
+    self.canvas_height = self.row * self.cell_size
+    self.canvas = tk.Canvas(parent, width=self.canvas_width, height=self.canvas_height)
+    self.canvas.pack()
 
-    # Create the Create new random field button
-    self.fieldbutton = tk.Button(self.container, text="Create new random field")
-    self.fieldbutton.pack(side="left")
-    self.fieldbutton.bind("<Button-1>", self.fieldbuttonClick) 
-    
-    # Create the Exit program button
-    self.exitbutton = tk.Button(self.container, text="Exit program")
-    self.exitbutton.pack(side="left")
-    self.exitbutton.bind("<Button-1>", self.exitbuttonClick)
-    
     # Create a 5x5 grid with random numbers
-    self.grid = [[random.randint(0, 9) for j in range(self.col)]
-                 for i in range(self.row)]
+    self.grid = [[random.randint(0, 9) for j in range(self.col)] for i in range(self.row)]
     self.grid[0][4] = 0
     self.grid[4][0] = 0
     self.grid[::-1]
-    self.canvas_collection = []
-    
-    self.buttons = []
+    self.rects = []
     for i in range(self.row):
-      row = []
-      for j in range(self.col):
-        button = tk.Button(self.root, text=self.grid[i][j])
-        button["width"] = 8
-        button["height"] = 3
-        button.grid(row=i, column=j)
-        row.append(button)
-      self.buttons.append(row)
-    self.buttons[4][0].configure(text="S")
-    self.buttons[0][4].configure(text="G")
+        row = []
+        for j in range(self.col):
+            x0 = j * self.cell_size
+            y0 = i * self.cell_size
+            x1 = x0 + self.cell_size
+            y1 = y0 + self.cell_size
+            rect = self.canvas.create_rectangle(x0, y0, x1, y1, fill="white")
+            text = self.canvas.create_text(x0+self.cell_size//2, y0+self.cell_size//2, text=str(self.grid[i][j]))
+            row.append((rect, text))
+        self.rects.append(row)
+    self.set_start(0, 4)
+    self.set_goal(4, 0)
     
-#    for i in range(self.row):
-#      row = []
-#      for j in range(self.col):
-#        canvas= tk.Canvas(self.frame, width= 8, height= 3, bg="White")
-#        canvas.create_text(4, 1.5, text=self.grid[i][j] ,fill="black",font=('Helvetica 15 bold'))
-#        row.append(canvas)
-#      self.canvas_collection.append(row)
-#    self.canvas_collection[4][0].configure(text="S")
-#    self.canvas_collection[0][4].configure(text="G")
+    # A container for the buttons
+    self.container1 = tk.Frame(parent)
+    self.container1.pack()
+    
+    # Print the maximum score
+    self.score_label = tk.Label(self.container1)
+    self.score_label["text"] = self.get_maximum_possible_score()
+    self.score_label.configure(text=self.score_label["text"])
+    self.score_label.pack(side="top")
+
+    #Button 1
+    self.button1 = tk.Button(self.container1, text="Find best path")
+    self.button1.pack(side="left")
+    self.button1.bind("<Button-1>", self.button1Click)
+
+    #Button 2
+    self.button2 = tk.Button(self.container1, text="Create new random field")
+    self.button2.pack(side="left")
+    self.button2.bind("<Button-1>", self.button2Click)
+
+    #Button 3
+    self.button3 = tk.Button(self.container1, text="Exit program")
+    self.button3.pack(side="left")
+    self.button3.bind("<Button-1>", self.button3Click)
+
+  def set_start(self, row, column):
+    rect, _ = self.rects[row][column]
+    self.canvas.itemconfig(rect, fill="green")
+
+  def set_goal(self, row, column):
+    rect, _ = self.rects[row][column]
+    self.canvas.itemconfig(rect, fill="red")
     
   @staticmethod
   def create_path_options():
@@ -128,14 +125,14 @@ class Grid:
                       best_root = path
           current_row = 4
           current_column = 0
-          result = self.grid[current_row][current_column]
+          result = self.grid[4][0]
+
       print(result_max, best_root)
       return result_max, best_root
 
   def get_maximum_possible_score(self):
     maximum_score = list(self.choose_best_root())[0]
     text = "Possible maximum score: " + str(maximum_score)
-    print(text)
     return text      
     
   def show_best_path(self):
@@ -146,11 +143,11 @@ class Grid:
     y_coordinates = [y_coordinate]
     for direction in best_path:
         if direction == "up":
-            y_coordinate -= 10
+            y_coordinate -= 60
             x_coordinates.append(x_coordinate)
             y_coordinates.append(y_coordinate)
         else:
-            x_coordinate += 10
+            x_coordinate += 60
             x_coordinates.append(x_coordinate)
             y_coordinates.append(y_coordinate)
     self.canvas = tk.Canvas(self.root)
@@ -162,12 +159,15 @@ class Grid:
                             x_coordinates[5], y_coordinates[5],
                             x_coordinates[6], y_coordinates[6],
                             x_coordinates[7], y_coordinates[7])
-    self.canvas.bind("<Button-1>", self.pathbuttonClick)
-    
-  def pathbuttonClick(self, event):
+    self.canvas.pack()
+    self.canvas.bind("<Button-1>", self.button1Click())
+  
+  # Button 1 draws the best path
+  def button1Click(self, event):
     self.show_best_path()
 
-  def fieldbuttonClick(self, event):
+  # Button 2 makes a new random field
+  def button2Click(self, event):
     self.grid = [[random.randint(0, 9) for j in range(self.col)]
                  for i in range(self.row)]
     for i in range(self.row):
@@ -176,16 +176,13 @@ class Grid:
     self.buttons[4][0].config(text="S")
     self.buttons[0][4].config(text="G")
 
-  # This button exits the program
-  def exitbuttonClick(self, event):
-    self.parent.destroy()
+  # Button 3 exits the program
+  def button3Click(self, event):
+    self.parent.destroy()  
 
-#print(Grid.get_maximum_possible_score())
 
 # Geometry and title
 root = tk.Tk()
-
-#print(Grid.choose_best_root())
 root.title("Best Path")
 root.geometry("500x500")
 find_best_path = Grid(root)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import tkinter as tk
 import random
 
 
-#Make class
+#Make class for the Pathfinder
 class Grid:
 
   def __init__(self, parent):
@@ -12,45 +12,72 @@ class Grid:
     self.row = 5
     self.col = 5
     self.root.pack()
+    self.cell_size = 60
+    self.canvas_width = self.col * self.cell_size
+    self.canvas_height = self.row * self.cell_size
+    self.canvas = tk.Canvas(parent,
+                            width=self.canvas_width,
+                            height=self.canvas_height)
+    self.canvas.pack()
 
     # Create a 5x5 grid with random numbers
     self.grid = [[random.randint(0, 9) for j in range(self.col)]
                  for i in range(self.row)]
-    self.buttons = []
+    self.rects = []
     for i in range(self.row):
       row = []
       for j in range(self.col):
-        button = tk.Button(self.root, text=self.grid[i][j])
-        button["width"] = 8
-        button["height"] = 3
-        button.grid(row=i, column=j)
-        row.append(button)
-      self.buttons.append(row)
-      
+        x0 = j * self.cell_size
+        y0 = i * self.cell_size
+        x1 = x0 + self.cell_size
+        y1 = y0 + self.cell_size
+        rect = self.canvas.create_rectangle(x0, y0, x1, y1, fill="white")
+        text = self.canvas.create_text(x0 + self.cell_size // 2,
+                                       y0 + self.cell_size // 2,
+                                       text=str(self.grid[i][j]))
+        row.append((rect, text))
+      self.rects.append(row)
+
+    # Create a 5x5 grid with random numbers
+    # self.grid = [[random.randint(0, 9) for j in range(self.col)]
+    #              for i in range(self.row)]
+    # self.buttons = []
+    # for i in range(self.row):
+    #   row = []
+    #   for j in range(self.col):
+    #     button = tk.Button(self.root, text=self.grid[i][j])
+    #     button["width"] = 8
+    #     button["height"] = 3
+    #     button.grid(row=i, column=j)
+    #     row.append(button)
+    #   self.buttons.append(row)
+
     # A container for the buttons
     self.container1 = tk.Frame(parent)
     self.container1.pack()
-    
+
     #Button 1
-    self.button1 = tk.Button(self.container1, text = "Find best path")
-    self.button1.pack(side = "left")
+    self.button1 = tk.Button(self.container1, text="Find best path", activebackground = "grey")
+    self.button1.pack(side="left")
     self.button1.bind("<Button-1>", self.button1Click)
-    
+
     #Button 2
-    self.button2 = tk.Button(self.container1, text="Create new random field")
-    self.button2.pack(side = "left")
+    self.button2 = tk.Button(self.container1, text="Create new random field", activebackground = "grey")
+    self.button2.pack(side="left")
     self.button2.bind("<Button-1>", self.button2Click)
 
     #Button 3
-    self.button3 = tk.Button(self.container1, text = "Exit program")
-    self.button3.pack(side = "left")
-    self.button3.bind("<Button-3>", self.button3Click)
+    self.button3 = tk.Button(self.container1, text="Exit program", activebackground = "red")
+    self.button3.pack(side="left")
+    self.button3.bind("<Button-1>", self.button3Click)
 
   def set_start(self, row, column):
-    self.buttons[row][column].config(text="S")
+    rect, _ = self.rects[row][column]
+    self.canvas.itemconfig(rect, fill="green")
 
   def set_goal(self, row, column):
-    self.buttons[row][column].config(text="G")
+    rect, _ = self.rects[row][column]
+    self.canvas.itemconfig(rect, fill="red")
 
   def calculate_maximal_score(self):
     pass
@@ -69,15 +96,18 @@ class Grid:
     for i in range(self.row):
       for j in range(self.col):
         self.buttons[i][j].config(text=self.grid[i][j])
+    self.buttons[4][0].config(text="S")
+    self.buttons[0][4].config(text="G")
 
   # Button 3 exits the program
-  def button3Click(self):
-    self.root.destroy()
+  def button3Click(self, event):
+    self.parent.destroy()
+
 
 # Geometry and title
 root = tk.Tk()
 root.geometry("500x500")
-root.title("Best Path")
+root.title("Pathfinder")
 
 # Create the grid within the frame (delete later when canvas works)
 Grid = Grid(root)
