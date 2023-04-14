d6e28bac2fbb4057e973046865442a3c57eef720
EvaTezeta <118466414+EvaTezeta@users.noreply.github.com>
2023-04-14 11:02:02 +0000

    Pop up window to quit and more comments

--- main.py
+++ main.py
@@ -31,6 +31,8 @@ class Grid:
         self.grid[0][4] = "G"
         self.grid[4][0] = "S"
         self.rects = []
+        
+        # TO DO: Add comments here to explain what it does
         for i in range(self.row):
             row = []
             for j in range(self.col):
@@ -57,38 +59,40 @@ class Grid:
         # A container for the labels
         self.container = tk.Frame(parent)
         self.container.pack()
-      
+
         # A container for the buttons
         self.container1 = tk.Frame(parent)
         self.container1.pack()
 
-        # Label: Print the maximum score label
+        # Label: Print the maximum score
         self.score_label = tk.Label(self.container, background="white")
         self.score_label["text"] = "Maximum possible score: "
         self.score_label.configure(text=self.score_label["text"])
         self.score_label.pack(side="top")
 
-        # Label: Print the maximum score obtained label
-        self.score_obtained_label = tk.Label(self.container, background="white")
+        # Label: Print the maximum score obtained by user
+        self.score_obtained_label = tk.Label(self.container,
+                                             background="white")
         self.score_obtained_label["text"] = "Maximum score obtained by you: "
-        self.score_obtained_label.configure(text=self.score_obtained_label["text"])
+        self.score_obtained_label.configure(
+            text=self.score_obtained_label["text"])
         self.score_obtained_label.pack(side="bottom")
 
-        #Button 1
+        #Button 1 - Best path button
         self.button1 = tk.Button(self.container1,
                                  text="Find best path",
                                  activebackground="grey")
         self.button1.pack(side="left")
         self.button1.bind("<Button-1>", self.button1Click)
 
-        #Button 2
+        #Button 2 - Random field button
         self.button2 = tk.Button(self.container1,
                                  text="Create new random field",
                                  activebackground="grey")
         self.button2.pack(side="left")
         self.button2.bind("<Button-1>", self.button2Click)
 
-        #Button 3
+        #Button 3 - Exit button
         self.button3 = tk.Button(self.container1,
                                  text="Exit program",
                                  activebackground='red')
@@ -100,42 +104,51 @@ class Grid:
 
     def cell_click(self, event):
 
-        #Change the color of the clicked cell to blue.
+        #Change the color of the clicked cell to yellow.
         x, y = event.x, event.y
         self.chosen_col = x // self.cell_size
         self.chosen_row = y // self.cell_size
         rect, _ = self.rects[self.chosen_row][self.chosen_col]
         self.canvas.itemconfig(rect, fill="yellow")
-#        self.user_score_maximum()
+        #        self.user_score_maximum() <- what's this?
         self.congratulate_user()
 
     def user_score_maximum(self):
+        # TO DO: Add comments here to explain what it does
         sum_of_the_grid = 0
         try:
-          self.numbers_in_the_grid.append(self.grid[self.chosen_row][self.chosen_col])
+            self.numbers_in_the_grid.append(
+                self.grid[self.chosen_row][self.chosen_col])
         except:
-          pass
+            pass
         for number in self.numbers_in_the_grid:
-          sum_of_the_grid += number
-        self.score_obtained_label["text"] = "Maximum score obtained by you: " + str(sum_of_the_grid)
+            sum_of_the_grid += number
+        self.score_obtained_label[
+            "text"] = "Maximum score obtained by you: " + str(sum_of_the_grid)
         return sum_of_the_grid
 
     def congratulate_user(self):
+        # TO DO: Add comments here to explain what it does
         self.click_counter += 1
         if list(self.choose_best_root())[0] == self.user_score_maximum():
             if self.click_counter == 9:
-                messagebox.showinfo("Maximum score check", "Congratulations! You got the maximum score!")
-  
+                messagebox.showinfo(
+                    "Maximum score check",
+                    "Congratulations! You got the maximum score!")
+
     def set_start(self, row, column):
+        # TO DO: Add comments here to explain what it does
         rect, _ = self.rects[row][column]
         self.canvas.itemconfig(rect, fill="red")
 
     def set_goal(self, row, column):
+        # TO DO: Add comments here to explain what it does
         rect, _ = self.rects[row][column]
         self.canvas.itemconfig(rect, fill="green")
 
     @staticmethod
     def create_path_options():
+        # TO DO: Add comments here to explain what it does
         root_1 = []
         root_2 = []
         root_3 = []
@@ -164,6 +177,7 @@ class Grid:
         return root_unique
 
     def choose_best_root(self):
+        # TO DO: Add comments here to explain what it does
         path_options = self.create_path_options()
         current_row = 4
         current_column = 0
@@ -191,6 +205,7 @@ class Grid:
         return result_max, best_root
 
     def show_best_path(self):
+        # TO DO: Add comments here to explain what it does
         best_path = list(self.choose_best_root())[1]
         x_coordinate = 30
         y_coordinate = 270
@@ -220,12 +235,12 @@ class Grid:
     # Button 1 draws the best path
     def button1Click(self, event):
         self.show_best_path()
+        # Displays the score of the best path in the label
         self.score_label["text"] = "Maximum possible score: " + str(
             list(self.choose_best_root())[0])
 
-    # Button 2 makes a new random field
+    # Button 2 generates a new random field
     def button2Click(self, event):
-        self.canvas.delete("best_path_line")
         self.grid = [[random.randint(0, 9) for j in range(self.col)]
                      for i in range(self.row)]
         self.grid[0][4] = "G"
@@ -235,7 +250,13 @@ class Grid:
                 self.canvas.itemconfig(self.rects[i][j][1],
                                        text=str(self.grid[i][j]))
                 rect, _ = self.rects[i][j]
+                # Resets chosen path colors back to white
                 self.canvas.itemconfig(rect, fill="white")
+                
+        # Deletes the best path line
+        self.canvas.delete("best_path_line")
+        
+        # Resets S & G to original colors       
         self.set_start(0, 4)
         self.set_goal(4, 0)
         self.grid[0][4] = 0
@@ -243,14 +264,15 @@ class Grid:
         self.grid[::-1]
         self.numbers_in_the_grid = []
         self.click_counter = 0
-        
+
         # Reset label text without score
         self.score_label["text"] = "Maximum possible score: "
         self.score_obtained_label["text"] = "Maximum score obtained by you: "
 
     # Button 3 exits the program
     def button3Click(self, event):
-        self.parent.destroy()
+        if messagebox.askyesno(title="Quit?", message="Do you really want to quit?"): 
+            self.parent.destroy()
 
 
 # Form window and title
